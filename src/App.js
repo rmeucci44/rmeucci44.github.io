45d36d8a44c2b0fc35dd96fad2846487e3944f33
Robbie <rmeucci@purdue.edu>
2025-07-10 21:06:54 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello World</h1>
+        <h2>ROBBIE PORTFOLIO</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
